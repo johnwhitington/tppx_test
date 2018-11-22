6ed75a5aecd95c6692a7edc14782e82a866635a1
John Whitington <john@coherentgraphics.co.uk>
2018-11-22 15:45:20 +0000

    more

--- tppx_test.ml
+++ tppx_test.ml
@@ -8,10 +8,20 @@ let newmapper argv =
        match expr with
        | {exp_attributes = [{attr_name = {txt = "swap"}}];
           exp_desc = Texp_tuple [exp_a; exp_b];
-          exp_type = {desc = Ttuple [typ_a; typ_b]}}
-            when typ_a.desc = typ_b.desc && Random.bool () ->
-              {expr with exp_desc = Texp_tuple [exp_b; exp_a]}
-       | other -> default.expr mapper other);
+          exp_type = {desc = Ttuple [typ_a; typ_b]} as typ_desc} ->
+            if typ_a.desc = typ_b.desc then
+              if Random.bool () then
+                {expr with
+                   exp_desc = Texp_tuple [exp_b; exp_a];
+                   exp_type = {typ_desc with desc = Ttuple [typ_b; typ_a]}}
+              else
+                default.expr mapper expr
+            else
+              begin
+                prerr_endline "Could not swap: types not alike";
+                default.expr mapper expr
+              end
+       | _ -> default.expr mapper expr);
   }
 
 let () =
